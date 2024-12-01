b502e4eb40a62112898d1d9011c458200dddae45
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-12-01 18:16:23 +0000

    add solution to task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- This script creates a tale users
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL
+)
+
